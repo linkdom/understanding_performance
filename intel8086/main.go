26ccb9f5b8f0a7df375dd94c3b4427932793a140
Dominik Link <vorbild94@gmail.com>
2024-04-16 21:35:31 +0200

    Reading binary file, putting them into variables and printing them

--- intel8086/main.go
+++ intel8086/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    file, err := os.Open("/home/dom/development/go/understanding_performance/intel8086/single_register_move")
+    if err != nil {
+         log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    binaries := make([]string, 2)
+
+    for scanner.Scan() {
+        values := scanner.Bytes()
+
+        for i, v := range values {
+            binaries[i] = fmt.Sprintf("%08b\n", v)
+        }
+    }
+
+    for _, x := range binaries {
+        fmt.Println(x)
+    }
+
+
+
+
 }
