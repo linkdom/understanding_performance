f45f97c417b1c5f30d1d25f388449fde56da0cb2
linkdom <Vorbild94@gmail.com>
2024-04-17 07:38:25 +0200

    read different bits and name them

--- intel8086/main.go
+++ intel8086/main.go
@@ -16,6 +16,7 @@ func main() {
 
     scanner := bufio.NewScanner(file)
     binaries := make([]string, 2)
+    assemblyInstructions := make([]string, 2)
 
     for scanner.Scan() {
         values := scanner.Bytes()
@@ -23,13 +24,22 @@ func main() {
         for i, v := range values {
             binaries[i] = fmt.Sprintf("%08b", v)
         }
+
+        opcode := binaries[0][0:6]
+        d := binaries[0][6]
+        w := binaries[0][7]
+        fmt.Println(opcode)
+        fmt.Println(string(d))
+        fmt.Println(string(w))
+
+        mod := binaries[1][0:2] 
+        reg := binaries[1][2:5]
+        rm := binaries[1][5:]
+        fmt.Println(mod)
+        fmt.Println(reg)
+        fmt.Println(rm)
     }
 
-    for _, x := range binaries {
-        fmt.Println(x)
-    }
-
-
-
+    fmt.Println(assemblyInstructions)
 
 }
